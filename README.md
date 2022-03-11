28819b9986c1991d1b88662700afb26dd25488bb
bronson-g <development@graansma.dev>
2022-03-11 16:27:57 -0500

    pass sudo to outside script caller

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tunebot api
 The API that handles manages users and their playlists for the [tunebot app](TBD) and [tunebot controller](https://github.com/cbeimers113/tunebot-controller).
+
+## Server setup
+1. clone this repository
+2. `sudo ./server.sh`
--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 cd ~/tunebot-api
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y make golang mysql-server
+apt update
+apt upgrade -y
+apt install -y make golang mysql-server
 git pull
 mysql --defaults-extra-file=/etc/mysql/debian.cnf < data/main.sql
-sudo make
+make
