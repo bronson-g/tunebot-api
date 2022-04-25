11af19fc9ae707ef27d5e425aaa8b63998ff4535
bronson-g <development@graansma.dev>
2022-04-25 19:29:11 -0400

    added logging for api endpoints

--- src/endpoint/shared.go
+++ src/endpoint/shared.go
@@ -1,11 +1,13 @@
 package endpoint
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 func successResponse(data []byte, w http.ResponseWriter) {
+	fmt.Println("\\e[32m" + string(data) + "\\e[39m")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
@@ -13,6 +15,7 @@ func successResponse(data []byte, w http.ResponseWriter) {
 
 func errorResponse(err error, w http.ResponseWriter) {
 	if err != nil {
+		fmt.Println("\\e[31m" + err.Error() + "\\e[39m")
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("{\"error\":\"" + strings.Replace(err.Error(), "\"", "\\\"", -1) + "\"}"))
